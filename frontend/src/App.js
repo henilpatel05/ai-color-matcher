a479bf96fea62c092b962f31985dfe79208819a1
henil patel <49230546+henilpatel05@users.noreply.github.com>
2025-02-17 12:44:59 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import Button from "./components/Button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Upload } from "lucide-react";
 
